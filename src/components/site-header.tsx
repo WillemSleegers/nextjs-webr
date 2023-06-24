7633627d4f024128a2b60f199825725ef99feb89
Willem Sleegers <w.sleegers@me.com>
2023-06-24 17:29:14 +0100

    Began adding components

---
+++ src/components/site-header.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { MainNav } from "./main-nav"
+
+export function SiteHeader() {
+  return (
+    <header className="z-40 w-full border-b bg-background">
+      <div className="mx-5 flex gap-3 h-16 items-center sm:justify-between">
+        <MainNav items={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-end">
+          <nav className="flex items-center mx-3 space-x-2">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.gitHub className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.twitter className="h-5 w-5 fill-current" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
